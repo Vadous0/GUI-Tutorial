dddb4b10a990d0c166367b204a4b988ffceafe39
Vadous0 <133013275+Vadous0@users.noreply.github.com>
2023-06-12 09:13:35 +0000

    Kommentare ergänzt

--- Main.java
+++ Main.java
@@ -1,23 +1,23 @@
 import javax.swing.*; // The package where the components are
 import java.awt.event.*; // Used for event listeners and events
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.GridLayout;
+import java.awt.BorderLayout; // For organazing Objeckts on a Layout
+import java.awt.GridLayout;  // For managing multiple objeckts on 1 frame
 
 public class Main {
   public static void main(String[] args) {
-
-    JPanel mainpanel = new JPanel(new GridLayout(4, 4));
+    
+    
     JFrame frame = new JFrame("My first SWing GUI");
-
     // Create a frame object
     frame.setSize(500, 500);
     // Set the size of the frame to 500 pixels in length and 500 pixels in height
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     // IMPORTANT. TYPE THIS IN EVERY FRAME OR ELSE THE CLOSE BUTTON WILL NOT DO
     // ANYTHING
+    JPanel mainpanel = new JPanel(new GridLayout(4, 4));
+    // Panel witch usually goes into a frame
+    
     JButton button = new JButton("Click me!");
-    button.setMaximumSize(new Dimension(10, 10));
     // Create an instance of JButton
     button.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
@@ -34,8 +34,10 @@ public class Main {
      */
 
     mainpanel.add(button, BorderLayout.WEST);
+    // adds the Button to the panel
+
     frame.getContentPane().add(mainpanel);
-    // Adds the button to the frame's content pane
+    // Adds the panel to the frame's content pane
     // Note: A frame in swing is just like a photoframe, it is the outside and holds
     // the content.
     // The Content Pane is where all the items go in or the "Picture in the frame"
